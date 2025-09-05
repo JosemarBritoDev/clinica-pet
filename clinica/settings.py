ed07d2cab9e0bce29a4a808d692df8aafdb4b7ad
Josemar Brito <estudos.josemarbrito@gmail.com>
2025-09-05 20:37:39 -0300

    Criado banco de dados com mysql e feito migração Close#10

--- clinica/settings.py
+++ clinica/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'clinica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'clinica_pet_db',
+        'USER': 'root',
+        'PASSWORD': 'clinica1305',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
