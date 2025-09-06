b89d293ee31f642b8d9b6514adc1abb210c93426
Josemar Brito <estudos.josemarbrito@gmail.com>
2025-09-06 16:25:24 -0300

    Primeiros passos na classe cliente

--- clinica/base/models.py
+++ clinica/base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+#from localflavor.br.br_states import STATE_CHOICES
 
-# Create your models here.
+class EnderecoCliente(models.Model):
+    rua = models.CharField(max_length=100, null=False, blank=False)
+    numero = models.IntegerField(null=False, blank=False)
+    complemento = models.CharField(max_length=100, null=False, blank=False)
+    bairro = models.CharField(max_length=100, null=False, blank=False)
+    cidade = models.CharField(max_length=100, null=False, blank=False)
+  #  estado = models.CharField(max_length=2, choices=STATE_CHOICES)
+    cep = models.CharField(max_length=100, null=False, blank=False)
+
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(max_length=100, null=False, blank=False)
+
