650e69da6d5ec89af1c07aeba7297234ec6c6e75
Josemar Brito <estudos.josemarbrito@gmail.com>
2025-09-09 12:38:41 -0300

    Criado modulo de clientes e formatados para pep8
    close #14

--- clinica/base/services/cliente_service.py
+++ clinica/base/services/cliente_service.py
@@ -2,5 +2,6 @@ from ..models import Cliente
 
 
 def cadastrar_cliente(cliente):
-    Cliente.objects.create(nome=cliente.nome, email=cliente.email, endereco=cliente.endereco, cpf=cliente.cpf,
-                           data_nascimento=cliente.data_nascimento, telefone=cliente.telefone)
+    Cliente.objects.create(nome=cliente.nome, email=cliente.email, endereco=cliente.endereco,
+                           cpf=cliente.cpf, data_nascimento=cliente.data_nascimento,
+                           telefone=cliente.telefone)
--- clinica/base/services/endereco_service.py
+++ clinica/base/services/endereco_service.py
@@ -2,6 +2,7 @@ from clinica.base.models import EnderecoCliente
 
 
 def cadastrar_endereco(endereco):
-    return EnderecoCliente.objects.create(rua=endereco.rua, numero=endereco.numero, complemento=endereco.complemento,
-                                          bairro=endereco.bairro, cidade=endereco.cidade, estado=endereco.estado,
-                                          cep=endereco.cep)
+    return EnderecoCliente.objects.create(rua=endereco.rua, numero=endereco.numero,
+                                          complemento=endereco.complemento,
+                                          bairro=endereco.bairro, cidade=endereco.cidade,
+                                          estado=endereco.estado, cep=endereco.cep)
